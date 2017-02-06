99a706a2a5bb9ce9e8c7d2981d053afe603557ea
podiumdesu <wwhmomo@gmail.com>
2017-02-06 18:04:24 +0800

    modified,but not finished

---
+++ task0007/homework2/5美化网页/js/friendrolls.js
@@ -0,0 +1,51 @@
+/**
+ * Created by petnakanojo on 1/31/17.
+ */
+//461.Hamming Distance
+//The Hamming distance between two integers is the number of positions
+//at which the corresponding bits are different.
+
+//Given two integers x and y, calculate the Hamming distance.
+
+//Note:
+//0 ≤ x, y < 231.
+
+//Example:
+//Input: x = 1, y = 4
+//Output: 2
+
+//Explanation:
+//1   (0 0 0 1)
+//4   (0 1 0 0)
+//       ↑   ↑
+
+//The above arrows point to positions where the corresponding bits are different.
+
+
+//ver1.0  使用位计算【异或】，一位位的比对该位是否是1，使用计数器进行计数
+
+var x = document.getElementById("field1").value;
+var y = document.getElementById("field2").value;
+
+var hammingDistance = function(x,y){
+    var count = 0;
+    var trans = x ^ y ;
+    while ( n> 0 ){
+        if ((n&1)==1){
+            c += 1;
+        }
+        n= n>>1;
+    }
+    alert(count);
+};
+
+button.onclick = function(event){
+    alert('ni');
+}
+
+document.write("I love JavaScript！");
+
+
+document.getElementsByClassName("demo1").
+document.getElementById("output").value= = count;
+document.getElementsByClassName("button").onclick = "alert(‘你的')";
